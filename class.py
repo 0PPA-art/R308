ee0a457d88a5a8112645a3509aa9ae84086c0558
e2506243 <sichardlomme@gmail.com>
2026-09-11 11:43:40 +0200

    push

--- class.py
+++ class.py
@@ -40,33 +40,36 @@ class Cercle:
         area = math.pi * rayon**2 # pi*r^2
         return area
 
-    def intersection(self, a : float, b : float, c:float, d:float)-> float :
-        rayon1 = math.sqrt((self.x - a)**2 + (self.y - b)**2)
-        rayon2 = math.sqrt((self.x - c)**2 + (self.y - d)**2)
-        d = math.sqrt((a - b)**2 + (c - d)**2)
-        if d > rayon1 + rayon2:
+    def intersection(self, a : float, b : float)-> float :
+        d = math.sqrt((a)**2 + (b)**2)
+        if d > a + b:
             msg = "il est en intersection : "
             return msg
+
+    def pointinstersection(self, a : float, b : float)-> float :
+        d = math.sqrt(a**2 + b**2)
+
+
 
 if __name__ == '__main__':
        p1 = point(3.2,1)
        print(p1)
-       p2 =point(5,5)
+       p2 =point(5.0,5.0)
        print(p2)
-       p3 = point(7.5,6.5)
+       p3 = point(2.5,6.5)
        print(p3)
-       p4 = point(2,3)
-       print(p4)
        print(f"{p1.distanceCoord(0,0): .2f}")
        print(f"{p2.distancePoint(p1): .2f}")
-       c1 = Cercle(0,0)
+       c1 = point(2,3)
        print(c1)
        #print(f"{c1.distancePoint( p2): .2f}")
        print(f"{Cercle.Diameter(c1, p2.x, p2.y): .2f}")
+       r1 = c1.distancePoint(p2)
+       r2 = c1.distancePoint(p3)
 
        print(f"{Cercle.perimeter(c1, p2.x, p2.y): .2f}")
        print(f"{Cercle.area(c1, p2.x, p2.y): .2f}")
-       print(f"{Cercle.intersection(c1, c1, c2): .2f}")
+       print(f"{Cercle.intersection(c1, r1, r2): .2f}")
 
 
 
